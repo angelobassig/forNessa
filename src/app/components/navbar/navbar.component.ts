4b82a962e6eba37e3c72b8f10700ec6e5fed432c
Angelo Bassig <angelo.bassig@pointwest.com.ph>
2022-03-15 22:54:16 +0800

    sendddddd

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { SessionService } from 'src/app/services/session.service';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css'],
+})
+export class NavbarComponent implements OnInit {
+  name: string = localStorage.getItem('name');
+  token: boolean = localStorage.getItem('token') !== null;
+
+  // March 14 2 pm add-ons
+  ownerUrl = localStorage.getItem('profileUrl');
+  // March 14 2 pm add-ons
+
+  constructor(private sessionService: SessionService, private router: Router) {
+    console.log("test")
+    console.log(this.token)
+  }
+
+  ngOnInit(): void {
+    this.sessionService.hasToken.subscribe((token) => {
+      this.token = token;
+      this.name = this.sessionService.getName();
+      console.log("reloaded navbar test")
+    });
+  }
+
+  logout() {
+    this.sessionService.clear();
+    this.ngOnInit();
+    this.router.navigate(['/login']);
+  }
+}
